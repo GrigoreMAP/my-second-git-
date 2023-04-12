aad49912e4f01f7cb0a317b03f69a358016c2a0f
Mihai <grigore.mihaita20@yahoo.com>
2023-04-12 11:44:26 +0300

    first commit

---
+++ my-first-git-project-main/src/main/java/calculator/Calculator.java
@@ -0,0 +1,23 @@
+package calculator;
+
+public class Calculator {
+    public int multiply(int a, int b, int c){
+        return a*b*c;
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public static double Divide(int var1, int var2) {
+
+         return var1/var2;
+
+    }
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+
+    public int sub(int a, int b){
+        return a-b;
+    }
+}
